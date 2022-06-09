879eb1b3afe692e719d5b1e6811fe4c8c5e83149
LinuxDeveloper <fariddevops22@gmail.com>
2022-06-09 10:12:13 +0200

    edited file

--- add-newer-local-user.sh
+++ add-newer-local-user.sh
@@ -7,16 +7,15 @@
 # The username, password, and host for the account will be displayed.
 
 # Make sure the script is being executed with superuser privileges.
-if [[ "${UID}" -ne 0 ]]
-then echo 'Please run with sudo or as root.' >&2
-exit 1
+if [[ "${UID}" -ne 0 ]]; then
+    echo 'Please run with sudo or as root.' >&2
+    exit 1
 fi
 
 # If the user doesn't supply at least one argument, then give them help.
-if [[ "${#}" -lt 1 ]]
-then
-echo "Usage: ${0} USER_NAME [COMMENT]..." >&2
-echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT.' >&2
+if [[ "${#}" -lt 1 ]]; then
+    echo "Usage: ${0} USER_NAME [COMMENT]..." >&2
+    echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT.' >&2
 fi
 
 # The first parameter is the user name
@@ -30,28 +29,26 @@ COMMENT="${@}"
 PASSWORD=$(date +%s%N | sha256sum | head -c48)
 
 # Create the user with the password.
-useradd -c "${COMMENT}" -m ${USER_NAME} &> /dev/null
+useradd -c "${COMMENT}" -m ${USER_NAME} &>/dev/null
 
 # Check to use if the useradd command succeeded.
 # We don't want to tell the user that an account was created it hasn't been.
-if [[ "${?}" -ne 0 ]]
-then
-echo 'The account could not be created.' >&2
-exit 1
+if [[ "${?}" -ne 0 ]]; then
+    echo 'The account could not be created.' >&2
+    exit 1
 fi
 
 # Set the password.
-echo ${PASSWORD} | passwd --stdin ${USER_NAME} &> /dev/null 
+echo ${PASSWORD} | passwd --stdin ${USER_NAME} &>/dev/null
 
 # Check to see if the passwd command succeeded.
-if [[ "${?}" -ne 0 ]]
-then
-echo 'The password for the account could not be set.' >&2
-exit 1
+if [[ "${?}" -ne 0 ]]; then
+    echo 'The password for the account could not be set.' >&2
+    exit 1
 fi
 
 # Force password change on first login.
-passwd -e ${USER_NAME} &> /dev/null
+passwd -e ${USER_NAME} &>/dev/null
 
 # Display the username, password, and the host where the user was created.
 echo 'username:'
@@ -66,4 +63,3 @@ echo 'host:'
 echo "${HOSTNAME}"
 echo
 exit 0
-
